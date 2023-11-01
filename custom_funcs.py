05dca6f4b3e5619be014dc019ddb290c9ce4f80d
Magica1ali <120230269+magica1ali@users.noreply.github.com>
2023-11-01 16:39:48 -0700

    Update custom_funcs.py

--- custom_funcs.py
+++ custom_funcs.py
@@ -325,7 +325,7 @@ def parse_clean_func(text_dict):
 filtered_sentences = spacyLayer(translated_text,corpus)
 st.write(filtered_sentences[0])
     #Extracts timestamps for topics over time visulization
-    @st.cache_data
+@st.cache_data
     def datetime_layer(text):
           
         # Create a list of dictionaries with 'sentence' and 'date' attributes
