f715f93c22a3aab1461bef851f2f4bf6ceb71051
Magica1ali <120230269+magica1ali@users.noreply.github.com>
2023-11-04 21:39:17 -0700

    Update visualization - topics & key terms.py

--- pages/visualization - topics & key terms.py
+++ pages/visualization - topics & key terms.py
@@ -7,5 +7,8 @@ from bertopic import BERTopic
 import matplotlib.pyplot as plt
 from sentence_transformers import SentenceTransformer
 
+st.title("BerTopic Visualizations : Topics & Key terms")
+
 st.write(st.session_state.fig0) # Topic Information 
 st.write(st.session_state.fig3) # Topic similarity
+st.write(st.session_state.fig4) # visualize the selected terms for a few topics
