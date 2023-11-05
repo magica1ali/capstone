57f3726fffbad6d94fc947e2168c9554e940731a
Magica1ali <120230269+magica1ali@users.noreply.github.com>
2023-11-04 23:46:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -66,11 +66,11 @@ def main():
                 st.write('Creating Visualizations...')
                 status.update(label="Process complete!", state="complete", expanded=False)
             
-                st.session_state.fig0 = topic_model.get_topic_info(filtered_sentences)
-                st.session_state.fig1 = topic_model.visualize_hierarchy(filtered_sentences)
-                st.session_state.fig2 = topic_model.visualize_topics(filtered_sentences)
-                st.session_state.fig3 = topic_model.visualize_heatmap(filtered_sentences)
-                st.session_state.fig4 = topic_model.visualize_barchart(filtered_sentences)
+                st.session_state.fig0 = topic_model.get_topic_info()
+                st.session_state.fig1 = topic_model.visualize_hierarchy()
+                st.session_state.fig2 = topic_model.visualize_topics()
+                st.session_state.fig3 = topic_model.visualize_heatmap()
+                st.session_state.fig4 = topic_model.visualize_barchart()
             custom_funcs.prove_success_func(topic_model)
 
         except Exception as e:
