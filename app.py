72a3b5413cad1e69f9916a2e1d8cd48b44f7f13d
Magica1ali <120230269+magica1ali@users.noreply.github.com>
2023-11-05 11:48:00 -0800

    Update app.py

--- app.py
+++ app.py
@@ -33,13 +33,13 @@ def main():
 
                 st.write('Parsing, spell-checking, and preprocessing recommendations text from PDFs...')
                 # Function to parse out the recommendations section, clean, and preprocess corpus
-                corpus,translated_text = custom_funcs.parse_clean_func(text_dict)
+                corpus,translated_text,sentences = custom_funcs.parse_clean_func(text_dict)
                 st.write(len(translated_text))
 
                 timestamps = custom_funcs.datetime_layer(translated_text)
                 st.write('Recommendations text preprocessed!')
                 st.write(timestamps)
-                st.write(translated_text[0])
+                st.write(sentences[0])
                 
                 time.sleep(1)
                 
