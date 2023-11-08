1dd0e1106788868ac2d18c1459c95e570ebcb509
ls1495 <120067084+ls1495@users.noreply.github.com>
2023-11-07 21:11:12 -0500

    ah

--- pages/visualization - Topics Over Time.py
+++ pages/visualization - Topics Over Time.py
@@ -10,6 +10,10 @@ from sentence_transformers import SentenceTransformer
 st.title("BerTopic Visualizations : Top 10 Topics Over Time")
 st.header('Displays the top 10 topics by frequency over the timespan of inputted reports')
 
+
+# Create a slider widget to control the number of topics to include
+num_topics = st.slider("Number of Topics to Include", 1, 10)
+    
 st.dataframe(st.session_state.topic_info)
 st.write(st.session_state.topic_info['Name'])
 st.write(st.session_state.fig5)
