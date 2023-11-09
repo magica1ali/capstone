b4176e2d1aa5ee8d1173d8ffc23b3f62486b1b79
ls1495 <120067084+ls1495@users.noreply.github.com>
2023-11-08 20:25:23 -0500

    new

--- app.py
+++ app.py
@@ -15,10 +15,9 @@ def main():
 
 
     #Import lovely Georgetown University logo
-    
     @st.cache_data
     def get_image():
-        url = "https://msb.georgetown.edu/wp-content/uploads/2022/06/gu_msob.vert_rgb.jpg"
+        url = "https://msb.georgetown.edu/wp-content/uploads/2022/08/GU_MSB_Transparent_Horizontal_Logo.png"
         r = requests.get(url)
         return BytesIO(r.content)
     
